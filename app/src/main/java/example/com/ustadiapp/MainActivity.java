acd808dcab5539fe4b578b4566aa4000b45ada71
naeemkhan12 <naeemb7070@gmail.com>
2017-11-29 23:56:48 +0500

    fix #3

--- app/src/main/java/example/com/ustadiapp/MainActivity.java
+++ app/src/main/java/example/com/ustadiapp/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
+
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 import com.firebase.ui.auth.AuthUI;
@@ -21,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
     private static final String LOG="TESTLOG";
     private static final int RC_SIGN_IN = 123;
 
+
+    private FirebaseAuth mAuth;
+
 // ...
 
     // Choose authentication providers
@@ -28,11 +33,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SignIn signIn = new SignIn();
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.main_frame,signIn);
-        fragmentTransaction.commit();
+        mAuth=FirebaseAuth.getInstance();
+        if(mAuth.getCurrentUser()==null){
+            SignIn signIn = new SignIn();
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.main_frame,signIn);
+            fragmentTransaction.commit();
+        }else {
+            Log.i(LOG,"User has Signed In");
+
+        }
 
 
     }
--- app/src/main/java/example/com/ustadiapp/SignInInterface.java
+++ app/src/main/java/example/com/ustadiapp/SignInInterface.java
@@ -5,4 +5,5 @@ package example.com.ustadiapp;
  */
 
 public interface SignInInterface {
+
 }
