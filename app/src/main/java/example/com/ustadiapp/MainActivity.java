e34099c8b323250a463149bcafe2b1eea9652420
naeemkhan12 <naeemb7070@gmail.com>
2017-11-29 18:34:45 +0500

    fix #4

--- app/src/main/java/example/com/ustadiapp/MainActivity.java
+++ app/src/main/java/example/com/ustadiapp/MainActivity.java
@@ -1,11 +1,16 @@
 package example.com.ustadiapp;
 
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
 import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.Arrays;
@@ -13,33 +18,56 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private static final String LOG="TESTLOG";
     private static final int RC_SIGN_IN = 123;
 
 // ...
 
     // Choose authentication providers
-    private List<AuthUI.IdpConfig> providers = Arrays.asList(
-            new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
-            new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build(),
-            new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build());
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FirebaseAuth auth = FirebaseAuth.getInstance();
-//        if(auth.getCurrentUser()!=null){
-//            setContentView(R.layout.activity_main);
-//        }else{
-            startActivityForResult(
-                    AuthUI.getInstance()
-                            .createSignInIntentBuilder()
-                            .setAvailableProviders(providers)
-                            .setTheme(R.style.GreenTheme)
-                            .build(),
-                    RC_SIGN_IN);
-//        }
+        setContentView(R.layout.activity_main);
+        SignIn signIn = new SignIn();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame,signIn);
+        fragmentTransaction.commit();
+
+
     }
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow.
+//        if (requestCode == RC_SIGN_IN) {
+//            IdpResponse response = IdpResponse.fromResultIntent(data);
+//            // Successfully signed in
+//            if (resultCode == RESULT_OK) {
+////                startActivity(SignedInActivity.createIntent(this, response));
+//                finish();
+//                return;
+//            } else {
+//                // Sign in failed
+//                if (response == null) {
+//                    // User pressed back button
+////                    showSnackbar(R.string.sign_in_cancelled);
+//                    return;
+//                }
+//
+//                if (response.getErrorCode() == ErrorCodes.NO_NETWORK) {
+////                    showSnackbar(R.string.no_internet_connection);
+////                    return;
+//                }
+//
+//                if (response.getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+////                    showSnackbar(R.string.unknown_error);
+////                    return;
+//                }
+//            }
+//
+////            showSnackbar(R.string.unknown_sign_in_response);
+//        }
+//    }
  }
 
 
---
+++ app/src/main/java/example/com/ustadiapp/SignInWithAuthUI.java
@@ -0,0 +1,56 @@
+package example.com.ustadiapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by naeem on 11/28/17.
+ */
+
+public class SignInWithAuthUI extends Fragment{
+    private static final int RC_SIGN_IN = 123;
+    private static final String LOG="TESTLOG";
+// ...
+
+    // Choose authentication providers
+    private List<AuthUI.IdpConfig> providers = Arrays.asList(
+            new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+            new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build(),
+            new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build());
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .setTheme(R.style.GreenTheme)
+                        .build(),
+                RC_SIGN_IN);
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.sample,container,false);
+    }
+}
