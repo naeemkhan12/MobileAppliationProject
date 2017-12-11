ab2375c11ac9d8e76f9b9ebe23a58f2897557b63
naeemkhan12 <naeemb7070@gmail.com>
2017-12-11 15:29:19 +0500

    implement FCM service

---
+++ app/src/main/java/example/com/ustadiapp/services/InstanceIdService.java
@@ -0,0 +1,23 @@
+package example.com.ustadiapp.services;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by naeem on 12/11/17.
+ */
+
+public class InstanceIdService extends FirebaseInstanceIdService {
+    private static final String LOG = "TESTLOG";
+    private String token;
+    @Override
+    public void onTokenRefresh() {
+        super.onTokenRefresh();
+        token = FirebaseInstanceId.getInstance().getToken();
+        Log.d(LOG,token);
+
+//        TODO: save token with userid on server
+    }
+}
