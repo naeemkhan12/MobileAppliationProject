30885d43861f1368555bb11feb3b8e8376844b3d
naeemkhan12 <naeemb7070@gmail.com>
2017-12-26 02:12:14 +0500

    login with email and password addition

--- app/src/main/java/example/com/ustadiapp/LoginActivity.java
+++ app/src/main/java/example/com/ustadiapp/LoginActivity.java
@@ -6,10 +6,17 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.ErrorCodes;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.ArrayList;
@@ -21,10 +28,15 @@ public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "TESTLOG";
     private static final int RC_SIGN_IN =123;
     private FirebaseAuth mAuth;
+    private Button logIn;
+    private TextView signUp;
+    private EditText email;
+    private EditText password;
 
     // Choose authentication providers
     private List<AuthUI.IdpConfig> providers = Arrays.asList(
             new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+            new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build(),
             new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build());
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,18 +49,55 @@ public class LoginActivity extends AppCompatActivity {
             startActivity();
 //            myRef = database.getReference("schedule/").child(mAuth.getCurrentUser().getUid());
         }else {
-            startActivityForResult(
-                    AuthUI.getInstance()
-                            .createSignInIntentBuilder()
-                            .setAvailableProviders(providers)
-                            .setTheme(R.style.signup_theme)
-                            .setLogo(R.drawable.ic_account_circle_white_48dp)
-                            .build(),
-                    RC_SIGN_IN);
+            setContentView(R.layout.auth);
+            logIn = (Button)findViewById(R.id.btn_login);
+            email = (EditText)findViewById(R.id.input_email);
+            password = (EditText)findViewById(R.id.input_password);
+            logIn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String emailStr=email.getText().toString();
+                    String passwordStr=password.getText().toString();
+                    // authenticate the user with firebase
+                    Log.i(TAG,emailStr+" "+passwordStr);
+                    authWithEmailPassword(emailStr,passwordStr);
+                }
+            });
+            signUp = (TextView) findViewById(R.id.link_signup);
+            signUp.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setAvailableProviders(providers)
+                                    .setTheme(R.style.signup_theme)
+                                    .setLogo(R.drawable.ic_account_circle_white_48dp)
+                                    .build(),
+                            RC_SIGN_IN);
+                }
+            });
         }
     }
 
+public void authWithEmailPassword(String email, String password){
+//    https://firebase.google.com/docs/auth/android/password-auth
+    mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+        @Override
+        public void onComplete(@NonNull Task<AuthResult> task) {
+            if (task.isSuccessful()){
+                Log.d(TAG, "createUserWithEmail:success");
+                startActivity();
+            }else {
+                Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                Toast.makeText(LoginActivity.this, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show();
 
+            }
+
+        }
+    });
+}
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
